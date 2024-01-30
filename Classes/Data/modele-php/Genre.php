f30736855762e2142784698194166909d32bb401
Bourreau Quentin <o22202632@info24-12.iut45.univ-orleans.fr>
2024-01-30 15:17:22 +0100

    feat: ajout classes FairePartie, Genre et Playlist en php

---
+++ Classes/Data/modele-php/Genre.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Class Genre
+ * Représente un genre musical avec ses propriétés et méthodes associées.
+ */
+class Genre
+{
+    /**
+     * @var int $id_genre L'identifiant unique du genre musical.
+     */
+    private $id_genre;
+
+    /**
+     * @var string $nom_genre Le nom du genre musical.
+     */
+    private $nom_genre;
+
+    /**
+     * Constructeur de la classe Genre.
+     *
+     * @param int    $id_genre  L'identifiant unique du genre musical.
+     * @param string $nom_genre Le nom du genre musical.
+     */
+    public function __construct(int $id_genre, string $nom_genre)
+    {
+        $this->id_genre = $id_genre;
+        $this->nom_genre = $nom_genre;
+    }
+
+    /**
+     * Obtient l'identifiant du genre musical.
+     *
+     * @return int L'identifiant du genre musical.
+     */
+    public function getIdGenre(): int
+    {
+        return $this->id_genre;
+    }
+
+    /**
+     * Obtient le nom du genre musical.
+     *
+     * @return string Le nom du genre musical.
+     */
+    public function getNomGenre(): string
+    {
+        return $this->nom_genre;
+    }
+
+    /**
+     * Modifie le nom du genre musical.
+     *
+     * @param string $nom_genre Le nouveau nom du genre musical.
+     */
+    public function setNomGenre(string $nom_genre): void
+    {
+        $this->nom_genre = $nom_genre;
+    }
+}
