2684015f1bb8d8ea01cac981e6d55860dbf181df
MaverickPrevost <maverickprevost@gmail.com>
2024-02-15 11:14:26 +0100

    feat : Ajout de la page pour genre/recherche

---
+++ static/script/genre2.js
@@ -0,0 +1,19 @@
+document.getElementById('buttonVoirPlus2').addEventListener('click', function() {
+    var song = document.querySelectorAll('.song');
+    var button = document.getElementById('voir2');
+    var icon = document.getElementById('icon2');
+
+    if (button.textContent === 'Voir plus') {
+        for (var i = 5; i < song.length; i++) {
+            song[i].style.display = 'block';
+        }
+        button.textContent = 'Voir moins';
+        icon.textContent = '-';
+    } else {
+        for (var i = 18; i < song.length; i++) {
+            song[i].style.display = 'none';
+        }
+        button.textContent = 'Voir plus';
+        icon.textContent = '+';
+    }
+});
