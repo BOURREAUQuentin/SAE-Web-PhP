c33e06fe98a4270c0a300faf309191939ca25893
Quentin_BOURREAU <quentin.bourreau@etu.univ-orleans.fr>
2024-02-20 22:25:09 +0100

    fix: suppression balises script pour systeme de likes et ajout fichier likes.js

--- templates/playlist.php
+++ templates/playlist.php
@@ -407,63 +407,14 @@ $les_filtres_annees = array("1970", "1980", "1990", "2000", "2010", "2020");
 	</section>
 	<script src="../static/script/search.js"></script>
     <script>
-        // Récupère tous les éléments avec l'ID "like"
-        const likeElements = document.querySelectorAll('#buttonfav');
-
-        // Ajoute un écouteur d'événements à chaque élément
-        likeElements.forEach(likeElement => {
-            likeElement.addEventListener('click', async (event) => {
-                // Vérifie si l'utilisateur est connecté
-                if (!<?php echo isset($utilisateur_connecte) ? 'true' : 'false' ?>) {
-                    // Redirige l'utilisateur vers la page de connexion
-                    window.location.href = '/?action=connexion_inscription';
-                    return;
-                }
-
-                const musiqueId = likeElement.value;
-                const isChecked = likeElement.classList.contains('background');
-
-                // Envoie une requête POST à la page actuelle
-                const response = await fetch(window.location.href, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: new URLSearchParams({
-                        musiqueId,
-                        isChecked,
-                    }),
-                });
-
-                // Appeler la fonction pour mettre à jour l'image
-                updateImageSource(!isChecked, likeElement);
-
-                // Vérifie si la requête a réussi
-                if (response.ok) {
-                    console.log('Like ajouté ou supprimé');
-                    // Ajoute ou supprime la classe "background" selon l'état précédent
-                    likeElement.classList.toggle('background');
-                } else {
-                    console.error('Erreur lors de la requête');
-                }
-            });
-        });
-
-        function updateImageSource(isLiked, buttonElement) {
-            const imgElement = buttonElement.querySelector('.fav');
-            if (isLiked) {
-                imgElement.src = '../static/images/fav_rouge.png';
-            } else {
-                imgElement.src = '../static/images/fav_noir.png';
-            }
-        }
-    </script>
-    <script>
         // Injecter les données JSON dans une variable JavaScript
         const musiques = <?php echo $musiques_playlist_json; ?>;
         // Injecter les données JSON dans une variable JavaScript
         const id_musiques = <?php echo $id_musiques_playlist_json; ?>;
+
+        const utilisateur_est_connecte = <?php echo isset($utilisateur_connecte) ? 'true' : 'false'; ?>;
     </script>
     <script src="../static/script/son.js"></script>
+    <script src="../static/script/likes.js"></script>
 </body>
 </html>
--- templates/recherche.php
+++ templates/recherche.php
@@ -343,56 +343,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <script src="../static/script/genre.js"></script>
 	<script src="../static/script/search.js"></script>
     <script>
-        // Récupère tous les éléments avec l'ID "like"
-        const likeElements = document.querySelectorAll('#buttonfav');
-
-        // Ajoute un écouteur d'événements à chaque élément
-        likeElements.forEach(likeElement => {
-            likeElement.addEventListener('click', async (event) => {
-                // Vérifie si l'utilisateur est connecté
-                if (!<?php echo isset($utilisateur_connecte) ? 'true' : 'false' ?>) {
-                    // Redirige l'utilisateur vers la page de connexion
-                    window.location.href = '/?action=connexion_inscription';
-                    return;
-                }
-
-                const musiqueId = likeElement.value;
-                const isChecked = likeElement.classList.contains('background');
-
-                // Envoie une requête POST à la page actuelle
-                const response = await fetch(window.location.href, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                    body: new URLSearchParams({
-                        musiqueId,
-                        isChecked,
-                    }),
-                });
-
-                // Appeler la fonction pour mettre à jour l'image
-                updateImageSource(!isChecked, likeElement);
-
-                // Vérifie si la requête a réussi
-                if (response.ok) {
-                    console.log('Like ajouté ou supprimé');
-                    // Ajoute ou supprime la classe "background" selon l'état précédent
-                    likeElement.classList.toggle('background');
-                } else {
-                    console.error('Erreur lors de la requête');
-                }
-            });
-        });
-
-        function updateImageSource(isLiked, buttonElement) {
-            const imgElement = buttonElement.querySelector('.fav');
-            if (isLiked) {
-                imgElement.src = '../static/images/fav_rouge.png';
-            } else {
-                imgElement.src = '../static/images/fav_noir.png';
-            }
-        }
+        const utilisateur_est_connecte = <?php echo isset($utilisateur_connecte) ? 'true' : 'false'; ?>;
     </script>
+    <script src="../static/script/likes.js"></script>
 </body>
 </html>
